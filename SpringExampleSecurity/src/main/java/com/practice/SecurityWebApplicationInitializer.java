f80d7a8f3a791f51f70dd0b2ddbb291daf1ab87a
maddy <maddy@LAPTOP-LFQHQP4A>
2021-06-02 02:31:23 +0530

    example1

---
+++ SpringExampleSecurity/src/main/java/com/practice/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.practice;
+
+import org.springframework.security.web.context.*;  
+
+public class SecurityWebApplicationInitializer  
+    extends AbstractSecurityWebApplicationInitializer {  
+  
+}  
---
+++ SpringExampleSecurity/src/main/java/com/practice/WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.practice;
+
+import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebSecurity
+@ComponentScan("com.practice")
+public class WebSecurityConfig implements WebMvcConfigurer {
+
+	@Bean
+	public UserDetailsService userDetailsService() throws Exception {
+		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+		manager.createUser(
+				User.withDefaultPasswordEncoder().username("javatpoint").password("java123").roles("USER").build());
+		return manager;
+	}
+
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.antMatcher("/").authorizeRequests().anyRequest().hasRole("ADMIN").and().httpBasic();
+	}
+}
